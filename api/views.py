40618edaed2648de8ff60fd8db4c13b56cf173f9
rachidboufous <rachidboufous32@gmail.com>
2022-02-06 21:54:50 +0100

    chrome driver

--- api/views.py
+++ api/views.py
@@ -96,7 +96,14 @@ def get_article_object(topic, article):
 
 def return_specific_articles(topic):
     articles_list = []
-    driver = webdriver.Chrome(".\chromedriver")
+    
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    
     topiclink = 'https://en.hespress.com/' + topic
     driver.get(topiclink)
     driver.execute_script("window.scrollTo(0,180000);")
